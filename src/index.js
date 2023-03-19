dc3aebe79211c5a24713b330e0e2bb3f9f33ad39
Andre Luis <andre.1357.luis@gmail.com>
2023-03-19 17:47:53 -0300

    🚧 htpp server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-// eslint-disable-next-line no-console
-console.log('hello');
+const PORT = 5000;
+
+async function handleRequest(request, response) {
+  response.setHeader('Content-Type', 'application/json');
+
+  if (request.url) {
+    if (request.url === '/authenticate') {
+      response.end('Hello API!');
+    }
+  }
+
+  response.end();
+}
+
+const app = http.createServer(handleRequest);
+
+app.listen(PORT, function listeningListener() {
+  console.log('server running at', app.address().port);
+});
