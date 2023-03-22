9b12d008cec9ce9f5ab16dc87ff1228346d764b1
Andre Luis <andre.1357.luis@gmail.com>
2023-03-22 14:59:35 -0300

    🔊 inject logger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,37 +1,47 @@
+const { runQueue } = require('./../worker');
+
 const queue = new Map();
 
 /**
- *
- * @param {import('http').IncomingMessage} request
- * @param {import('http').ServerResponse} response
- * @returns {Promise<void>}
+ * @param {import('pino').Logger} logger
  */
-async function handleRequest(request, response) {
-  response.setHeader('Content-Type', 'application/json');
+function buildRequest(logger) {
+  /**
+   * @param {import('http').IncomingMessage} request
+   * @param {import('http').ServerResponse} response
+   * @returns {Promise<void>}
+   */
+  return async function handleRequest(request, response) {
+    response.setHeader('Content-Type', 'application/json');
 
-  const routeKey = request.url + ':' + request.method;
+    const routeKey = request.url + ':' + request.method;
 
-  if (routeKey === '/send-message:GET') {
-    for (let i = 0; i < 100; i++) {
-      queue.set(i, 'pong');
+    if (routeKey === '/send-message:GET') {
+      for (let i = 0; i < 100; i++) {
+        queue.set(i, 'pong');
+      }
+
+      const result = await runQueue(queue);
+
+      logger.info({ result }, 'result');
+
+      response.write('Message has been successfully sent!');
+
+      return response.end();
     }
 
-    response.write('Message has been successfully sent!');
+    if (routeKey === '/authenticate:GET') {
+      response.write('Authenticate has been successfully!');
+      return response.end();
+    }
 
-    return response.end();
-  }
-
-  if (routeKey === '/authenticate:GET') {
-    response.write('Authenticate has been successfully!');
-    return response.end();
-  }
-
-  response.write('Welcome to the King of Zap Zap!');
-  response.end();
+    response.write('Welcome to the King of Zap Zap!');
+    response.end();
+  };
 }
 
 function onStop(callback) {
   callback(queue);
 }
 
-module.exports = { handleRequest, onStop };
+module.exports = { buildRequest, onStop };
