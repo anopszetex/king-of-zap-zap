51a40a440106a2a06e8ca07a15180e4085c1c7c0
Andre Luis <andre.1357.luis@gmail.com>
2023-03-22 14:38:19 -0300

    🎨 improve structure

--- src/index.js
+++ src/index.js
@@ -3,40 +3,16 @@ require('dotenv').config();
 const closeWithGrace = require('close-with-grace');
 const http = require('node:http');
 
+const { handleRequest, onStop } = require('./routes');
 const { logger } = require('./support');
-
-Error.stackTraceLimit = 0; //* 0 to turn off stack traces
 
 const PORT = process.env.PORT ?? 5000;
 
-const queue = new Map();
-
-async function handleRequest(request, response) {
-  const routeKey = request.url + ':' + request.method;
-
-  response.setHeader('Content-Type', 'application/json');
-
-  if (routeKey === '/authenticate:GET') {
-    response.write('Authenticate has been successfully!');
-    return response.end();
-  }
-
-  if (routeKey === '/send-message:GET') {
-    for (let i = 0; i < 100; i++) {
-      queue.set(i, 'pong');
-    }
-
-    response.write('Message has been successfully sent!');
-
-    return response.end();
-  }
-
-  response.write('Welcome to the King of Zap Zap!');
-  response.end();
-}
+Error.stackTraceLimit = 0; //* 0 to turn off stack traces
 
 const app = http.createServer(handleRequest);
 
+//* start server
 app.listen(PORT, function listeningListener() {
   logger.info(`🚀 Server running at: http://localhost:${PORT}`);
 });
@@ -44,15 +20,22 @@ app.listen(PORT, function listeningListener() {
 //* error handling
 app.on('error', function listeningError(err) {
   if (err.code === 'EADDRINUSE') {
-    logger.error({ errorData: err }, 'Port: ' + PORT + ' is already in use');
+    logger.fatal({ errorData: err }, 'Port: ' + PORT + ' is already in use');
   }
 });
 
 //* graceful shutdown
-closeWithGrace({ delay: 500 }, function ({ signal }) {
-  logger.info({ signal }, 'Finalizing application');
-  queue.clear();
+closeWithGrace({ delay: 500 }, function listeningCloseWithGrace({ signal }) {
+  logger.debug({ signal }, 'Finalizing application');
+
+  onStop(queue => {
+    logger.debug({ size: queue.size }, 'Queue size');
+    queue.clear();
+    logger.debug({ queue }, 'Queue has been successfully cleared');
+  });
+
   app.closeIdleConnections();
   app.close();
-  logger.info('Application has been successfully close');
+
+  logger.debug('Application has been successfully close');
 });
