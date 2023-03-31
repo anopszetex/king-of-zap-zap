a6793d1c581676e8a5c6b2040ebfc9a86ac65fa5
Andre Luis <andre.1357.luis@gmail.com>
2023-03-30 23:18:16 -0300

    💡 remove comments

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ const functionBody = `return new Promise(resolve => setTimeout(resolve, value))`
 const sleep = new Function(...functionArgs, functionBody);
 
 async function processQueue(jobs) {
-  await sleep(); //
+  await sleep();
 
   parentPort.postMessage(jobs.keys().next().value); //* row = [ '852c3f22', 'test@test.com' ]
 }
