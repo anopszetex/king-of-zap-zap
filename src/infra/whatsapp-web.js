8cd7b405ab0802f961fbc0fed8fc45271261bd17
Andre Luis <andre.1357.luis@gmail.com>
2023-04-23 13:33:46 -0300

    ♻️ improve builder

--- src/infra/whatsapp-web.js
+++ src/infra/whatsapp-web.js
@@ -1,12 +1,35 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LegacySessionAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-function factory(logger) {
-  const client = new Client();
+const path = require('node:path');
+const fs = require('node:fs');
 
+const SESSION_FILE_PATH = path.join(__dirname, 'session.json');
+
+const options = { session: null };
+
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  options.session = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  authStrategy: new LegacySessionAuth(options),
+});
+
+client.on('authenticated', session => {
+  options.session = session;
+
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
+
+function factory() {
   return {
     init() {
-      client.initialize();
+      return client.initialize();
     },
     qrcodeGen() {
       client.on('qr', qr => {
@@ -14,16 +37,14 @@ function factory(logger) {
       });
     },
     ready() {
-      client.on('ready', () => {
-        logger.info('Client is ready!');
+      return new Promise((resolve, reject) => {
+        client.on('ready', resolve);
+        client.on('auth_failure', reject);
+        client.on('disconnected', reject);
       });
     },
-    send(value = 'pong') {
-      client.on('message', msg => {
-        if (msg.body === '!ping') {
-          client.sendMessage(msg.from, value);
-        }
-      });
+    async send(value = 'pong', number = '') {
+      client.sendMessage(number, value);
     },
   };
 }
