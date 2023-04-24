b750c711ff2a8cd11fa22f19e9a18366d408d1db
Andre Luis <andre.1357.luis@gmail.com>
2023-04-23 21:35:41 -0300

    ✨ delegated message to thread

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,7 @@
 const { parentPort, threadId } = require('node:worker_threads');
+const { factory } = require('./infra/whatsapp-web');
+
+const client = factory();
 
 const functionArgs = ['value=500'];
 const functionBody = `return new Promise(resolve => setTimeout(resolve, value))`;
@@ -7,6 +10,12 @@ const sleep = new Function(...functionArgs, functionBody);
 
 async function processQueue(_) {
   await sleep();
+
+  client.init();
+  client.generateQrCode();
+
+  await client.ready();
+  await client.send();
 
   parentPort.postMessage(
     'Thread: ' + threadId + ' has been successfully processed!'
